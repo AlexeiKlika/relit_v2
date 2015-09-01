164afa1f752efb9e75bd1430f0534196ff5ee28f
Alexei Klika <AlexeiKlika@users.noreply.github.com>
2015-09-01 07:34:34 -0500

    Add bootstrap and start gulp.js config

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,19 @@
 var gulp = require('gulp'),
-	concat = require('gulp-concat');
+	concat = require('gulp-concat'),
+	fileinclude = require('gulp-file-include'),
+	del = require('del');
 
-gulp.task('default', function() {
-  // place code for your default task here
+gulp.task('clean', function(){
+	del(['./build/*'])
 });
+
+gulp.task('build-home', function() {
+  return gulp.src(['index.html'])
+  	.pipe(fileinclude({
+  		prefix: '@@',
+  		basepath:'@file'
+  	})).
+  	.pipe.gulp.dest("./build/")
+});
+
+gulp.task('default', ['build-home']);
