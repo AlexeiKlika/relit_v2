3ba5d580baf32caf5674b950998f76ba19cb0490
Alexei Klika <AlexeiKlika@users.noreply.github.com>
2015-09-17 13:22:57 -0500

    remove video bg, replace with static bg

--- relittraining.com/js/script.js
+++ relittraining.com/js/script.js
@@ -24,26 +24,6 @@ $(document).ready(function(){
 	    event.preventDefault();
 	});
 
-	// Mute Volume
-	$("#video-bg-volume span").click(function() {
-		var $v = $('#main-video-bg');
-		$v.attr('muted', 'false');
-		if ($v[0].muted == true) {
-			$v[0].muted = false;
-			$v[0].volume = 0;
-			$v.animate({volume: 1}, 1500);
-			$(this).removeClass('glyphicon-volume-off');
-			$(this).addClass('glyphicon-volume-up');
-		} else {
-			$v.animate({volume: 0}, 300);
-			$(this).removeClass('glyphicon-volume-up');
-			$(this).addClass('glyphicon-volume-off');
-			window.setTimeout(function(){
-				$v[0].muted = true;
-			}, 300);
-		}
-	});
-
 	$(".thumb-container").click(function() {
 		console.log('thumb container clicked...')
 		var go_to = $(this).attr('go-video');
@@ -59,6 +39,29 @@ $(document).ready(function(){
 		}
 	});
 
-
 });
+
+/*
+// Mute Volume
+$("#video-bg-volume span").click(function() {
+	var $v = $('#main-video-bg');
+	$v.attr('muted', 'false');
+	if ($v[0].muted == true) {
+		$v[0].muted = false;
+		$v[0].volume = 0;
+		$v.animate({volume: 1}, 1500);
+		$(this).removeClass('glyphicon-volume-off');
+		$(this).addClass('glyphicon-volume-up');
+	} else {
+		$v.animate({volume: 0}, 300);
+		$(this).removeClass('glyphicon-volume-up');
+		$(this).addClass('glyphicon-volume-off');
+		window.setTimeout(function(){
+			$v[0].muted = true;
+		}, 300);
+	}
+});
+
+
+*/
 
